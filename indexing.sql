a8c8779edeacb1e2d3efee1396493fe3b6376e95
Jamie Yamashita <jamieyamashita@Jamies-MBP-2.hawaii.rr.com>
2018-09-09 17:53:11 -1000

    Finished Indexing

--- indexing.sql
+++ indexing.sql
@@ -9,3 +9,16 @@ WHERE make_code='NISSAN' AND model_code='GT-R';
 --4ms
 
 SELECT make_code, model_code, model_title
+FROM car_models
+WHERE make_code='LAM';
+--8ms
+
+SELECT *
+FROM car_models
+WHERE year BETWEEN 2010 AND 2015;
+--36ms
+
+SELECT *
+FROM car_models
+WHERE year='2010';
+--9ms
